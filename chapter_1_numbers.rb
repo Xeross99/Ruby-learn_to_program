2ed043c8938ef75a9569995a7972e76ce6a89c45
Xeross99 <m.krzysteczko@icloud.com>
2023-10-31 10:07:23 +0100

    chapter 1 done

--- chapter_1_numbers.rb
+++ chapter_1_numbers.rb
@@ -1,1 +1,13 @@
-puts 1+2
+# Excercises
+
+# 1. How many hours are in the year.
+puts 365 * 24
+
+# 2. How many minutes are in a decade. (10 years)
+puts 365 * 10 * 24 * 60
+
+# 3. your age in seconds
+puts 24 * 365 * 24 * 60 * 60 
+
+# 4. Author's age.
+puts 1390000000 / 60 / 60 / 24 / 365
