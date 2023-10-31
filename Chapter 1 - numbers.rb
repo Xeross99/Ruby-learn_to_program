5a3548664219854519c5de56cd9dbd89c6090d15
Xeross99 <m.krzysteczko@icloud.com>
2023-10-31 10:12:07 +0100

    rename

---
+++ Chapter 1 - numbers.rb
@@ -0,0 +1,13 @@
+# Excercises
+
+# 1. How many hours are in the year.
+puts 365 * 24
+
+# 2. How many minutes are in a decade. (10 years)
+puts 365 * 10 * 24 * 60
+
+# 3. your age in seconds
+puts 24 * 365 * 24 * 60 * 60 
+
+# 4. Author's age.
+puts 1390000000 / 60 / 60 / 24 / 365
